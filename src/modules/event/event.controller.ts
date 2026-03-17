77bff9dbf1fa55ac030c3aaf60d254e43c90cb65
kimjs <kimjs910522@gmail.com>
2026-03-17 18:26:24 +0900

    ✨ feat : Event API 작업

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from 'express';
 import { mask } from 'superstruct';
 import eventStruct from './event.validation';
+import eventService from './event.service';
 
 class EventController {
   // 이벤트 목록 조회
   getEventList = async (req: Request, res: Response) => {
-    console.log('hello event');
+    const query = mask(req.query, eventStruct.getEventList);
+    const userId = req.user!.id;
+
+    const eventList = await eventService.getEventList(query, userId);
+    res.status(200).json(eventList);
   };
 }
 
--- src/modules/event/event.validation.ts
+++ src/modules/event/event.validation.ts
@@ -3,8 +3,8 @@ import commonStruct from '../../structs/common.validation';
 
 const getEventList = s.object({
   apartmentId: commonStruct.uuid,
-  year: commonStruct.number,
-  month: commonStruct.number,
+  year: s.refine(commonStruct.number, 'year-range', (val) => val >= 2000 && val <= 3000),
+  month: s.refine(commonStruct.number, 'month-range', (val) => val >= 1 && val <= 12),
 });
 
 export default {
